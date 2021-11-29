0df1afe538b7ae33c6fb4331dc64bfb15f60ee28
Nixon1863 <c.nixon@2018.ljmu.ac.uk>
2021-11-29 12:16:17 +0000

    Integrated Jest for unit testing

---
+++ src/test/pages/createPatient.test.js
@@ -0,0 +1,5 @@
+const { default: createPatient } = require("../../main/pages/createPatient");
+
+test("Creates a patient when appropriate details are provided", ()=>{
+    expect(createPatient()).toBe();
+});
